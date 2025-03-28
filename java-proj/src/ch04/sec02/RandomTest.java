4f569ba6013a5036b5a5c76f34497dbc6b60c5f9
jiwon214 <amliy0310415@gmail.com>
2025-03-28 18:41:27 +0900

    제

---
+++ java-proj/src/ch04/sec02/RandomTest.java
@@ -0,0 +1,15 @@
+package ch04.sec02;
+
+public class RandomTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int d = (int)(Math.random() *6)+1;
+		
+		System.out.println("난수 : "+d);
+		
+	
+	}
+
+}
